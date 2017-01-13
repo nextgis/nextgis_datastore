0fcff61e472cc8baec64fd9e3e6ff532aca5567b
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-14 01:27:50 +0300

    Fix android toolchain commands

--- ci/travis/android/install.sh
+++ ci/travis/android/install.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-wget -N -P $HOME/downloads https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
+wget -c -N -P $HOME/downloads https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
 unzip -q $HOME/downloads/android-ndk-r13b-linux-x86_64.zip -d $HOME/android-ndk
-$HOME/android-ndk/android-ndk-r13b/build/tools/make-standalone-toolchain.sh --arch arm --api 21 --install-dir=$HOME/android-toolchain
+$HOME/android-ndk/android-ndk-r13b/build/tools/make-standalone-toolchain.sh --arch=arm --platform=android-8 --install-dir=$HOME/android-toolchain
