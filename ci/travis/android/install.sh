e0275567892655d9ac96ab2414521a49ca553086
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-14 01:16:00 +0300

    Fix android-ndk paths

--- ci/travis/android/install.sh
+++ ci/travis/android/install.sh
@@ -4,5 +4,4 @@ set -e
 
 wget -N -P $HOME/downloads https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
 unzip -q $HOME/downloads/android-ndk-r13b-linux-x86_64.zip -d $HOME/android-ndk
-ls $HOME/android-ndk
-$HOME/android-ndk/build/tools/make-standalone-toolchain.sh --arch arm --api 21 --install-dir=$HOME/android-toolchain
+$HOME/android-ndk/android-ndk-r13b/build/tools/make-standalone-toolchain.sh --arch arm --api 21 --install-dir=$HOME/android-toolchain
