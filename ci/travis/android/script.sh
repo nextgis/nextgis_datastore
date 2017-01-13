c718face41b3946eefbc469376909e97541dc557
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-14 02:59:58 +0300

    Fix android script.sh in ci

--- ci/travis/android/script.sh
+++ ci/travis/android/script.sh
@@ -8,6 +8,6 @@ export PATH=$HOME/android-8-toolchain/bin:$PATH
 
 mkdir build
 cd build
-CC="arm-linux-androideabi-gcc" CXX="arm-linux-androideabi-g++" cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=ANDROID $HOME/android-toolchain/bin # TODO: -DBUILD_TESTING=ON -DBUILD_TESTS=ON
+CC="arm-linux-androideabi-gcc" CXX="arm-linux-androideabi-g++" cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=ANDROID -DCMAKE_BUILD_TYPE=Release -DANDROID_STANDALONE_TOOLCHAIN=$HOME/android-toolchain .. # TODO: -DBUILD_TESTING=ON -DBUILD_TESTS=ON
 cmake --build . --config release -- -j 4
 # TODO: cmake --build . --config release --target test
