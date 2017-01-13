d008ce00ba86236183a1301cc2012347abe7e9fe
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-14 01:12:45 +0300

    Cache download. Fix paths

--- ci/travis/android/install.sh
+++ ci/travis/android/install.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-wget https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
-unzip -q android-ndk-r13b-linux-x86_64.zip
-android-ndk-r13b-linux-x86_64/build/tools/make-standalone-toolchain.sh --arch arm --api 21 --install-dir=$HOME/android-toolchain
+wget -N -P $HOME/downloads https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
+unzip -q $HOME/downloads/android-ndk-r13b-linux-x86_64.zip -d $HOME/android-ndk
+ls $HOME/android-ndk
+$HOME/android-ndk/build/tools/make-standalone-toolchain.sh --arch arm --api 21 --install-dir=$HOME/android-toolchain
