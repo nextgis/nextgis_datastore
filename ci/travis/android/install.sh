89b0286c5945dae9a5b3572ef29895626b60fa56
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-14 01:32:27 +0300

    Some help output from ci

--- ci/travis/android/install.sh
+++ ci/travis/android/install.sh
@@ -5,3 +5,4 @@ set -e
 wget -c -N -P $HOME/downloads https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
 unzip -q $HOME/downloads/android-ndk-r13b-linux-x86_64.zip -d $HOME/android-ndk
 $HOME/android-ndk/android-ndk-r13b/build/tools/make-standalone-toolchain.sh --arch=arm --platform=android-8 --install-dir=$HOME/android-toolchain
+ls -lh $HOME/android-toolchain
