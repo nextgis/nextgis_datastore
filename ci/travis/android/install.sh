1402f7350b8c9d465601651595d1b7669929e24d
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-14 02:27:32 +0300

    Fix install toolchain path in ci

--- ci/travis/android/install.sh
+++ ci/travis/android/install.sh
@@ -7,7 +7,7 @@ wget -c -N -P $HOME/downloads https://dl.google.com/android/repository/android-n
 echo "Extract files"
 unzip -q $HOME/downloads/android-ndk-r13b-linux-x86_64.zip -d $HOME/android-ndk
 echo "Make toolchain"
-$HOME/android-ndk/android-ndk-r13b/build/tools/make-standalone-toolchain.sh --system=linux-x86_64 --platform=android-8 --install-dir="$HOME/android-toolchain"
+$HOME/android-ndk/android-ndk-r13b/build/tools/make-standalone-toolchain.sh --toolchain=x86_64-linux-android --platform=android-8 --install-dir="$HOME/android-toolchain"
 ls -lh $HOME
 echo "List of $HOME/android-toolchain"
 ls -lh $HOME/android-toolchain
