a713a5b6a3b420f9f6d10e3d824c012133b19966
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-13 13:22:23 +0300

    Fix script build command in ci

--- ci/travis/linux_gcc/script.sh
+++ ci/travis/linux_gcc/script.sh
@@ -4,6 +4,6 @@ set -e
 
 mkdir build
 cd build
-cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON
+cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON ..
 cmake --build . --config release -- -j 4
 cmake --build . --config release --target test
