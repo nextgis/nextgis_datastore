42727da3c05faa979398365b2c02bc7863b8b5d2
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-13 17:15:25 +0300

    Add linux clang

---
+++ ci/travis/linux_clang/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+echo "Add NextGIS PPA"
+sudo add-apt-repository -y ppa:nextgis/ppa
+sudo apt-get update -qq
+echo "Install dependencies"
+sudo apt-get install -qq cmake swig libgdal-dev zlib1g-dev liblzma-dev libgeos-dev libjpeg-dev libproj-dev libexpat-dev libpng-dev libgeotiff-dev libtiff5-dev libcurl4-openssl-dev libxml2-dev liblzma-dev libsqlite3-dev libjson-c-dev libgles2-mesa-dev
---
+++ ci/travis/linux_clang/script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+mkdir build
+cd build
+cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON ..
+cmake --build . --config release -- -j 4
+cmake --build . --config release --target test
