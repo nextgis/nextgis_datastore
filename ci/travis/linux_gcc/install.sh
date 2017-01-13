5b2df7ec3481d952d2c87ecbfe2c04136cf375fb
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-13 13:04:07 +0300

    Fix install.sh in ci

--- ci/travis/linux_gcc/install.sh
+++ ci/travis/linux_gcc/install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "Add NextGIS PPA"
 sudo add-apt-repository -y ppa:nextgis/ppa
 sudo apt-get update -qq
-sudo apt-get install cmake swig libgdal-dev zlib1g-dev liblzma-dev libgeos-dev libjpeg-dev libproj-dev libexpat-dev libpng-dev libgeotiff-dev libtiff5-dev libcurl-ssl-dev libxml2-dev liblzma-dev libsqlite3-dev libjson-c-dev
+echo "Install dependencies"
+sudo apt-get install -qq cmake swig libgdal-dev zlib1g-dev liblzma-dev libgeos-dev libjpeg-dev libproj-dev libexpat-dev libpng-dev libgeotiff-dev libtiff5-dev libcurl-ssl-dev libxml2-dev liblzma-dev libsqlite3-dev libjson-c-dev
