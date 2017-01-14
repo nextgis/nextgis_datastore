ac0d3cec90b5e586309f8a6803fb4c819307efce
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-14 05:17:22 +0300

    Fix osx ci

--- ci/travis/osx/install.sh
+++ ci/travis/osx/install.sh
@@ -5,11 +5,11 @@ set -e
 brew update
 brew install swig
 
-git clone https://github.com/nextgis-borsch/lib_jsonc.git
-git clone https://github.com/nextgis-borsch/lib_geos.git
-git clone https://github.com/nextgis-borsch/lib_proj.git
-git clone https://github.com/nextgis-borsch/lib_jpeg.git
-git clone https://github.com/nextgis-borsch/lib_png.git
-git clone https://github.com/nextgis-borsch/lib_tiff.git
-git clone https://github.com/nextgis-borsch/lib_geotiff.git
-git clone https://github.com/nextgis-borsch/lib_gdal.git
+git clone https://github.com/nextgis-borsch/lib_jsonc.git $HOME/nextgis/lib_jsonc
+git clone https://github.com/nextgis-borsch/lib_geos.git $HOME/nextgis/lib_geos
+git clone https://github.com/nextgis-borsch/lib_proj.git $HOME/nextgis/lib_proj
+git clone https://github.com/nextgis-borsch/lib_jpeg.git $HOME/nextgis/lib_jpeg
+git clone https://github.com/nextgis-borsch/lib_png.git $HOME/nextgis/lib_png
+git clone https://github.com/nextgis-borsch/lib_tiff.git $HOME/nextgis/lib_tiff
+git clone https://github.com/nextgis-borsch/lib_geotiff.git $HOME/nextgis/lib_geotiff
+git clone https://github.com/nextgis-borsch/lib_gdal.git $HOME/nextgis/lib_gdal
--- ci/travis/osx/script.sh
+++ ci/travis/osx/script.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-cd $HOME/lib_jsonc
+cd $HOME/nextgis/lib_jsonc
 mkdir build
 cd build
-cmake -DOSX_FRAMEWORK=ON -DREGISTER_PACKAGE=ON -DSUPPRESS_VERBOSE_OUTPUT=ON -DCMAKE_INSTALL_PREFIX=$HOME/lib_jsonc/inst ..
+cmake -DOSX_FRAMEWORK=ON -DREGISTER_PACKAGE=ON -DSUPPRESS_VERBOSE_OUTPUT=ON -DCMAKE_INSTALL_PREFIX=$HOME/nextgis/lib_jsonc/inst ..
 cmake --build . --config release -- -j 4
 cmake --build . --config release --target install
 
-cd $HOME/lib_geos
+cd $HOME/nextgis/lib_geos
 mkdir build
 cd build
-cmake -DOSX_FRAMEWORK=ON -DREGISTER_PACKAGE=ON -DSUPPRESS_VERBOSE_OUTPUT=ON -DCMAKE_INSTALL_PREFIX=$HOME/lib_geos/inst ..
+cmake -DOSX_FRAMEWORK=ON -DREGISTER_PACKAGE=ON -DSUPPRESS_VERBOSE_OUTPUT=ON -DCMAKE_INSTALL_PREFIX=$HOME/nextgis/lib_geos/inst ..
 cmake --build . --config release -- -j 4
 cmake --build . --config release --target install
 
-cd $HOME/nextgis_datastore
+cd $HOME/nextgis/nextgis_datastore
 mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON ..
