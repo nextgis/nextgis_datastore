01a43b7a10b1a6e08f2e7446035eab5cbb484132
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-14 03:14:09 +0300

    Fix toolchain path in ci

--- ci/travis/android/install.sh
+++ ci/travis/android/install.sh
@@ -6,4 +6,4 @@ wget -c -N -P $HOME/downloads https://dl.google.com/android/repository/android-n
 echo "Extract files"
 unzip -q $HOME/downloads/android-ndk-r13b-linux-x86_64.zip -d $HOME/android-ndk
 echo "Make toolchain"
-python $HOME/android-ndk/android-ndk-r13b/build/tools/make_standalone_toolchain.py --arch x86_64 --api 21 --install-dir $HOME/android-toolchain
+python $HOME/android-ndk/android-ndk-r13b/build/tools/make_standalone_toolchain.py --arch arm --api 21 --install-dir $HOME/android-toolchain
--- ci/travis/android/script.sh
+++ ci/travis/android/script.sh
@@ -8,6 +8,6 @@ export PATH=$HOME/android-toolchain/bin:$PATH
 
 mkdir build
 cd build
-cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=ANDROID -DCMAKE_BUILD_TYPE=Release -DANDROID_STANDALONE_TOOLCHAIN=$HOME/android-toolchain -DANDROID_ABI=x86_64 .. # TODO: -DBUILD_TESTING=ON -DBUILD_TESTS=ON CC="arm-linux-androideabi-gcc" CXX="arm-linux-androideabi-g++"
+cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=ANDROID -DCMAKE_BUILD_TYPE=Release -DANDROID_STANDALONE_TOOLCHAIN=$HOME/android-toolchain  .. # TODO: -DBUILD_TESTING=ON -DBUILD_TESTS=ON CC="arm-linux-androideabi-gcc" CXX="arm-linux-androideabi-g++" -DANDROID_ABI=x86_64
 cmake --build . --config release -- -j 4
 # TODO: cmake --build . --config release --target test
