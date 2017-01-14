c821565d146d535e0aabee47a27367d6dc69c58f
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-14 03:05:35 +0300

    Fix toolchain path in ci

--- ci/travis/android/script.sh
+++ ci/travis/android/script.sh
@@ -4,10 +4,10 @@ set -e
 
 unset CC
 unset CXX
-export PATH=$HOME/android-8-toolchain/bin:$PATH
+export PATH=$HOME/android-toolchain/bin:$PATH
 
 mkdir build
 cd build
-CC="arm-linux-androideabi-gcc" CXX="arm-linux-androideabi-g++" cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=ANDROID -DCMAKE_BUILD_TYPE=Release -DANDROID_STANDALONE_TOOLCHAIN=$HOME/android-toolchain .. # TODO: -DBUILD_TESTING=ON -DBUILD_TESTS=ON
+cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=ANDROID -DCMAKE_BUILD_TYPE=Release -DANDROID_STANDALONE_TOOLCHAIN=$HOME/android-toolchain -DANDROID_ABI=x86_64 .. # TODO: -DBUILD_TESTING=ON -DBUILD_TESTS=ON CC="arm-linux-androideabi-gcc" CXX="arm-linux-androideabi-g++"
 cmake --build . --config release -- -j 4
 # TODO: cmake --build . --config release --target test
