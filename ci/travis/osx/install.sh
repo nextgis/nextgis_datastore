b584f5fdf080f5eff7288283b2f15b70c941f2ca
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-16 11:42:41 +0300

    Add curl option keys

--- ci/travis/osx/install.sh
+++ ci/travis/osx/install.sh
@@ -5,19 +5,16 @@ set -e
 brew update
 brew install swig
 
-git clone https://github.com/nextgis-borsch/lib_jsonc.git $HOME/nextgis/lib_jsonc
-git clone https://github.com/nextgis-borsch/lib_geos.git $HOME/nextgis/lib_geos
-git clone https://github.com/nextgis-borsch/lib_proj.git $HOME/nextgis/lib_proj
-git clone https://github.com/nextgis-borsch/lib_jpeg.git $HOME/nextgis/lib_jpeg
-git clone https://github.com/nextgis-borsch/lib_png.git $HOME/nextgis/lib_png
-git clone https://github.com/nextgis-borsch/lib_tiff.git $HOME/nextgis/lib_tiff
-git clone https://github.com/nextgis-borsch/lib_geotiff.git $HOME/nextgis/lib_geotiff
-git clone https://github.com/nextgis-borsch/lib_openssl.git $HOME/nextgis/lib_openssl
-git clone https://github.com/nextgis-borsch/lib_curl.git $HOME/nextgis/lib_curl
-git clone https://github.com/nextgis-borsch/lib_gdal.git $HOME/nextgis/lib_gdal
+git clone https://github.com/nextgis-borsch/lib_jsonc.git $HOME/build/nextgis/lib_jsonc
+git clone https://github.com/nextgis-borsch/lib_geos.git $HOME/build/nextgis/lib_geos
+git clone https://github.com/nextgis-borsch/lib_proj.git $HOME/buildnextgis/lib_proj
+git clone https://github.com/nextgis-borsch/lib_jpeg.git $HOME/build/nextgis/lib_jpeg
+git clone https://github.com/nextgis-borsch/lib_png.git $HOME/build/nextgis/lib_png
+git clone https://github.com/nextgis-borsch/lib_tiff.git $HOME/build/nextgis/lib_tiff
+git clone https://github.com/nextgis-borsch/lib_geotiff.git $HOME/build/nextgis/lib_geotiff
+git clone https://github.com/nextgis-borsch/lib_openssl.git $HOME/build/nextgis/lib_openssl
+git clone https://github.com/nextgis-borsch/lib_curl.git $HOME/build/nextgis/lib_curl
+git clone https://github.com/nextgis-borsch/lib_gdal.git $HOME/build/nextgis/lib_gdal
 
 echo "List directory"
-ls -lh $PWD
-ls -lh $HOME
-ls -lh $HOME/nextgis
-ls -lh $HOME/build
+ls -lh $HOME/build/nextgis
