074eefef0f740e4befe55f4ee1bac64efa4f2e39
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-16 04:28:48 +0300

    Build with updated version of libraries

--- ci/travis/android/install.sh
+++ ci/travis/android/install.sh
@@ -3,6 +3,7 @@
 set -e
 
 sudo apt-get install -qq cmake swig python 
+mkdir -p $HOME/downloads
 wget -c -N -P $HOME/downloads https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
 echo "Extract files"
 unzip -q $HOME/downloads/android-ndk-r13b-linux-x86_64.zip -d $HOME/android-ndk
--- ci/travis/android/script.sh
+++ ci/travis/android/script.sh
@@ -7,7 +7,7 @@ set -e
 export PATH=$HOME/android-toolchain/bin:$PATH
 export ANDROID_STANDALONE_TOOLCHAIN=$HOME/android-toolchain
 
-mkdir build
+mkdir -p build
 cd build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=ANDROID -DCMAKE_BUILD_TYPE=Release -DANDROID_STANDALONE_TOOLCHAIN=$HOME/android-toolchain  .. # TODO: -DBUILD_TESTING=ON -DBUILD_TESTS=ON CC="arm-linux-androideabi-gcc" CXX="arm-linux-androideabi-g++" -DANDROID_ABI=x86_64
 cmake --build . --config release -- -j 4
