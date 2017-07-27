cef15ad2febc73c42308449a189eccd22796a8ca
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-07-27 14:39:25 +0300

    Fix travis ci

--- ci/travis/linux_gcc/script.sh
+++ ci/travis/linux_gcc/script.sh
@@ -5,11 +5,11 @@ set -e
 mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON ..
-cmake --build . --config release -- -j 4
-echo "execute ./test/matrix_test"
-./test/matrix_test
+cmake --build . --clean-first --config release -- -j 4
 echo "execute ./test/store_test"
 ./test/store_test
+echo "execute ./test/matrix_test"
+./test/matrix_test
 echo "execute ./test/main_test"
 ./test/main_test
 cmake --build . --config release --target test
