20bb3992b5c036e64bf411f52b2c72a2e1d12d63
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-07-27 14:18:57 +0300

    Fix travis ci

--- ci/travis/linux_gcc/script.sh
+++ ci/travis/linux_gcc/script.sh
@@ -7,6 +7,7 @@ cd build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON ..
 cmake --build . --config release -- -j 4
 ls
-ls ./lib
 ls ./test
+./test/main_test
+ls ./test/data
 cmake --build . --config release --target test
