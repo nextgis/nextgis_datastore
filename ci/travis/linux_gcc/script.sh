e697f4d1bc6e0c85bc901d7b310dda23f31ebf94
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-07-27 22:41:16 +0300

    Test trevis ci

--- ci/travis/linux_gcc/script.sh
+++ ci/travis/linux_gcc/script.sh
@@ -6,6 +6,7 @@ mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON ..
 cmake --build . --clean-first --config release -- -j 4
+echo "execute ./test/main_test"
 cd ./test
 echo "execute ./test/main_test"
 ./main_test
