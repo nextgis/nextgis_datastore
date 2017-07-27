31012e3d23db7330f1eb63ca2d519e69f8e45766
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-07-28 00:25:01 +0300

    Test trevis ci

--- ci/travis/osx/script.sh
+++ ci/travis/osx/script.sh
@@ -116,8 +116,8 @@ cd $HOME/build/nextgis/nextgis_datastore
 mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON ..
-cmake --build . --config release -- -j 4cd
-./test
+cmake --build . --config release -- -j 4
+cd ./test
 echo "execute ./test/main_test"
 ./main_test
 cd ..
