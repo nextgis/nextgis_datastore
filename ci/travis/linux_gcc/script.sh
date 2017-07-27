324c2e37638d75617532fcc0b5f4ac939b4aeaef
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-07-27 14:09:56 +0300

    Fix travis ci

--- ci/travis/linux_gcc/script.sh
+++ ci/travis/linux_gcc/script.sh
@@ -6,4 +6,5 @@ mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON ..
 cmake --build . --config release -- -j 4
+./lib/test/main_test
 cmake --build . --config release --target test
--- ci/travis/osx/install.sh
+++ ci/travis/osx/install.sh
@@ -14,7 +14,7 @@ git clone --depth 1 https://github.com/nextgis-borsch/lib_tiff.git $HOME/build/n
 git clone --depth 1 https://github.com/nextgis-borsch/lib_geotiff.git $HOME/build/nextgis/lib_geotiff
 git clone --depth 1 https://github.com/nextgis-borsch/lib_openssl.git $HOME/build/nextgis/lib_openssl
 git clone --depth 1 https://github.com/nextgis-borsch/lib_curl.git $HOME/build/nextgis/lib_curl
-git clone --depth 1 https://github.com/nextgis-borsch/lib_gdal.git $HOME/build/nextgis/lib_gdal
+git clone --depth 1 -b dev https://github.com/nextgis-borsch/lib_gdal.git $HOME/build/nextgis/lib_gdal
 git clone --depth 1 https://github.com/nextgis-borsch/lib_sqlite.git $HOME/build/nextgis/lib_sqlite
 #git clone --depth 1 https://github.com/nextgis-borsch/lib_boost.git $HOME/build/nextgis/lib_boost
 #git clone --depth 1 https://github.com/nextgis-borsch/lib_cgal.git $HOME/build/nextgis/lib_cgal
