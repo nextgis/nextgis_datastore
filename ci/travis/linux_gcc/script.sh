05d532c3a95d3beca6636e47e043eff3f4dc512d
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-09-12 10:42:52 +0300

    Trevis tune

--- ci/travis/linux_gcc/script.sh
+++ ci/travis/linux_gcc/script.sh
@@ -6,6 +6,7 @@ mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON ..
 cmake --build . --config release -- -j 4
-cd test
-./main_test
+#cd test
+#./main_test
+#cd ..
 cmake --build . --config release --target test
