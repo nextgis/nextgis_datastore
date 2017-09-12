8433bbd50815e17351225afd34c9a0d3f03939f7
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-09-12 18:12:27 +0300

    Add verbose output

--- ci/travis/linux_gcc/script.sh
+++ ci/travis/linux_gcc/script.sh
@@ -6,7 +6,8 @@ mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON ..
 cmake --build . --config release -- -j 4
-cd test
-./main_test
-cd ..
-cmake --build . --config release --target test
+# cd test
+# ./main_test
+# cd ..
+# cmake --build . --config release --target test
+ctest -V
