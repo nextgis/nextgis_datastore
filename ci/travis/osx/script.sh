725df9b11fb4d4cfa28a972c5ee8bf96860b2e31
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-09-23 12:48:09 +0300

    Detail output for unit tests under mac os x

--- ci/travis/osx/script.sh
+++ ci/travis/osx/script.sh
@@ -117,4 +117,5 @@ mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TARGET_PLATFORM=Desktop -DBUILD_TESTING=ON -DBUILD_TESTS=ON ..
 cmake --build . --config release -- -j 4
-cmake --build . --config release --target test
+#cmake --build . --config release --target test
+ctest -V
