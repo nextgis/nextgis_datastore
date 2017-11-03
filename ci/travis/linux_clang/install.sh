b2dbaf6062018463ebaa7d40c8d168483d34085f
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-11-03 17:50:04 +0300

    Remove boost cgal from install

--- ci/travis/linux_clang/install.sh
+++ ci/travis/linux_clang/install.sh
@@ -9,6 +9,8 @@ sudo add-apt-repository -y ppa:teward/swig3.0
 
 echo "Add NextGIS PPA"
 sudo add-apt-repository -y ppa:nextgis/dev
+sudo add-apt-repository -y ppa:nextgis/ppa
 sudo apt-get update -qq
 echo "Install dependencies"
-sudo apt-get install -qq cmake swig3.0 libgdal-dev zlib1g-dev liblzma-dev libgeos-dev libjpeg-dev libproj-dev libexpat1-dev libpng-dev libgeotiff-dev libtiff5-dev libcurl4-openssl-dev libxml2-dev liblzma-dev libsqlite3-dev libjson-c-dev libgles2-mesa-dev libboost-dev libboost-system-dev libboost-thread-dev libcgal-dev
+sudo apt-get install -qq cmake swig3.0 libgdal-dev zlib1g-dev liblzma-dev libgeos-dev libjpeg-dev libproj-dev libexpat1-dev libpng-dev libgeotiff-dev libtiff5-dev libcurl4-openssl-dev libxml2-dev liblzma-dev libsqlite3-dev libjson-c-dev libgles2-mesa-dev
+# libboost-dev libboost-system-dev libboost-thread-dev libcgal-dev
