35d2d2a47df5857b4042c3dfa9ba7a8d2994d3ef
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-09-12 23:19:43 +0300

    Update ci

--- ci/travis/linux_clang/install.sh
+++ ci/travis/linux_clang/install.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Add NextGIS PPA"
 sudo add-apt-repository -y ppa:nextgis/dev
-# sudo add-apt-repository -y ppa:nextgis/ppa
+sudo add-apt-repository -y ppa:nextgis/ppa
 sudo apt-get update -qq
 echo "Install dependencies"
 sudo apt-get install -qq cmake libgdal-dev zlib1g-dev liblzma-dev libgeos-dev libjpeg-dev libproj-dev libexpat1-dev libpng-dev libgeotiff-dev libtiff5-dev libcurl4-openssl-dev libxml2-dev liblzma-dev libsqlite3-dev libjson-c-dev libgles2-mesa-dev
