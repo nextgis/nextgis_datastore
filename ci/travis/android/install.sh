3bb884e8a8ab3f0d6ed5d685177db6e9225a4b41
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-09-12 11:18:45 +0300

    Update ci

--- ci/travis/android/install.sh
+++ ci/travis/android/install.sh
@@ -6,17 +6,12 @@ set -e
 # http://askubuntu.com/questions/575505/glibcxx-3-4-20-not-found-how-to-fix-this-error
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
-# swig3.0 v3.02 for Ubuntu 12.04
-sudo add-apt-repository -y ppa:teward/swig3.0
-# swig3.0 v3.08 for Ubuntu 14.04
-#sudo add-apt-repository -y ppa:nschloe/swig-backports
-
 sudo apt-get update -qq
-sudo apt-get install -qq libstdc++6 swig3.0
+sudo apt-get install -qq libstdc++6
 
 mkdir -p $HOME/downloads
-wget -c -N -P $HOME/downloads https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
+wget -c -N -P $HOME/downloads https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip
 echo "Extract files"
-unzip -q $HOME/downloads/android-ndk-r13b-linux-x86_64.zip -d $HOME/android-ndk
+unzip -q $HOME/downloads/android-ndk-r17c-linux-x86_64.zip -d $HOME/android-ndk
 echo "Make toolchain"
-python $HOME/android-ndk/android-ndk-r13b/build/tools/make_standalone_toolchain.py --arch arm --api 21 --install-dir $HOME/android-toolchain
+python $HOME/android-ndk/android-ndk-r17c/build/tools/make_standalone_toolchain.py --arch arm --api 21 --install-dir $HOME/android-toolchain
